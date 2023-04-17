91d16739b11e25072d204c3c076439c30c2b6d5b
Shivanath Muthoju <shivanathmuthoju@gmail.com>
2023-04-17 13:57:50 +0530

    Config

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const express = require('express')
+const express = require('express');
+const app = express();
+const port = 8000;
+
+const db = require('./config/mongoose')
+app.use(express.urlencoded({extended : false}));
+
+app.listen(port, () => {
+    console.log(`Server is up and running on port: ${port}`)
+})
