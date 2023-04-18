a73d129c396eedc25575602b28ec4b4484f6c395
Shivanath Muthoju <shivanathmuthoju@gmail.com>
2023-04-19 02:01:37 +0530

    Signing pages and welcome pages partial setup

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,3 +1,7 @@
 module.exports.home = (req, res) => {
-    res.render('../views/home.ejs', {title : "Hey"})
+    res.render('home.ejs', {title : "Hey"})
+}
+
+module.exports.welcome = (req, res) => {
+    res.render('welcome.ejs')
 }
--- routes/signup.js
+++ routes/signup.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const signUpController = require('../controllers/singupController');
 
 router.get('/', signUpController.signup);
-
+router.post('/', (req, res) => {
+    console.log(req.query)
+    res.send('<h1>Hey</h1>')
+})
 module.exports = router;
